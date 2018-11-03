675022cd69ed087a309ca50f4686ebffc83a9e09
Aimini <2026084033@qq.com>
2018-11-03 22:25:13 +0800

    server basic function was completed

--- src/main/kotlin/com/win_vkey/startai/server/keyContoller.kt
+++ src/main/kotlin/com/win_vkey/startai/server/keyContoller.kt
@@ -1,38 +1,51 @@
 package com.win_vkey.startai.server
 
-import org.springframework.web.bind.annotation.PostMapping
+import org.apache.tomcat.jni.Buffer
+import org.apache.tomcat.util.http.fileupload.IOUtils
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
+import java.io.*
+import java.nio.charset.Charset
+import javax.annotation.PostConstruct
 
 @RestController
 @RequestMapping("key-down")
 class KeyController {
-    @RequestMapping
-    public fun key(KeyCode: Int): String {
-        //TODO: execute python script to send key
-        val cmd = """cmd /c dir c:\windows""" //raw string with three "
-        val process = Runtime.getRuntime().exec(cmd)
-        printMessage(process.inputStream)
-        printMessage(process.errorStream)
-        val value = process.waitFor()
+    private var pyProcess: Process = Runtime.getRuntime().exec("""python""")
+    private var pyProcessReader:BufferedReader = BufferedReader(InputStreamReader(pyProcess.inputStream))
+    private var pyProcessWriter: OutputStreamWriter = OutputStreamWriter(pyProcess.outputStream)
 
-        return "key down(code):$KeyCode"
+    @PostConstruct
+    fun loadProcess() {
+        pyProcess = Runtime.getRuntime().exec("""python ./.extract/SendKey.py""")
+        pyProcessReader = BufferedReader(InputStreamReader(pyProcess.inputStream))
+        pyProcessWriter = OutputStreamWriter(pyProcess.outputStream)
+
     }
 
-    private fun printMessage(input: InputStream) {
-        Thread(Runnable {
-            val reader = InputStreamReader(input)
-            val bf = BufferedReader(reader)
-            var line: String?
-            while(true) {
-                line = bf.readLine()
-                if(line == null)
-                    break
-                println(line)
-            }
-        }).start();
+    @RequestMapping
+    public fun key(KeyCode: Int): String {
+        //use sync for single user and low concurrency,(I'm so slothful)
+        synchronized(this) {
+            var result: String = ""
+            val write = Thread(Runnable {
+                pyProcessWriter.apply {
+                    write(KeyCode.toString() + "\n")
+                    flush()
+                }
+            })
+            write.start()
+            write.join()
+
+            val read = Thread(Runnable {
+                result = pyProcessReader.readLine()
+
+            })
+
+            read.start()
+            read.join()
+            print(result)
+        }
+        return "key down(code):$KeyCode"
     }
 }
--- src/main/resources/extract/SendKey.py
+++ src/main/resources/extract/SendKey.py
@@ -8,15 +8,15 @@ VK_MEDIA_PLAY_PAUSE = 0xB3
 def main():
     while True:
         try:
-            a = input("enter your key code:")
 
+            a = input("enter your key code:")
             if a.upper().startswith('0X'):
                 a = int(a, 16)
             else:
                 a = int(a, 10)
             win32api.keybd_event(a, 0, 0, 0)
             win32api.keybd_event(a, 0, win32con.KEYEVENTF_KEYUP, 0)
-            print("0")
+            print(a)
         except Exception as e:
             print(e)
             break
