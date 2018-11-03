9e43f7bb4f005ce63dd868f162138547911ab455
Aimini <2026084033@qq.com>
2018-11-03 20:50:41 +0800

    "send key" python script finshed

--- src/main/resources/extract/SendKey.py
+++ src/main/resources/extract/SendKey.py
@@ -1,3 +1,26 @@
-#TODO: use win32 api to send key
-print("python")
-print("new line")
+# TODO: use win32 api to send key
+import win32api
+import win32con
+
+VK_MEDIA_PLAY_PAUSE = 0xB3
+
+
+def main():
+    while True:
+        try:
+            a = input("enter your key code:")
+
+            if a.upper().startswith('0X'):
+                a = int(a, 16)
+            else:
+                a = int(a, 10)
+            win32api.keybd_event(a, 0, 0, 0)
+            win32api.keybd_event(a, 0, win32con.KEYEVENTF_KEYUP, 0)
+            print("0")
+        except Exception as e:
+            print(e)
+            break
+
+
+if __name__ == '__main__':
+    main()
